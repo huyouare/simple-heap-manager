9e36ceefe77b71c21b76d307659f68f5a791bab2
Jesse Hu <jessehu@wl-10-190-52-60.wireless.duke.edu>
2014-01-24 02:09:08 -0500

    Initial commit

---
+++ README.txt
@@ -0,0 +1,62 @@
+/**********************************************
+ * Please DO NOT MODIFY the format of this file
+ **********************************************/
+
+/*************************
+ * Team Info & Time spent
+ *************************/
+
+	Name1: Full Name 	// Edit this accordingly
+	NetId1: fn	 	// Edit
+	Time spent: 10 hours 	// Edit 
+
+/******************
+ * Files to submit
+ ******************/
+
+	dmm.c 	// Header file is not necessary
+	README	// This file filled with the lab implementation details
+
+/************************
+ * Implementation details
+ *************************/
+
+/* 
+ * This section should contain the implementation details and a overview of the
+ * results. 
+
+ * You are required to provide a good README document including the
+ * implementation details. In particular, you can use pseudocode to describe your
+ * implementation details where necessary. However that does not mean to
+ * copy/paste your C code. Rather, describe the evaluation choices you made in
+ * the design of your heap manager.  For example, it should contain about the
+ * list of information you store in the metadata structure, how the coalesce,
+ * free, and split operations benefited from the metadata structure, the space
+ * and time overheads and tradeoffs, the results of your test cases, the amount
+ * of time you spend on the lab, your name and any collaborators involved,
+ * references to any additional resources used, and your feedback on the lab).
+
+ * We expect the design and implementation details to be at most 1-2 pages.  A
+ * plain textfile is encouraged. However, a pdf is acceptable. No other forms
+ * are permitted.
+
+ * In case of lab is limited in some functionality, you should provide the
+ * details to maximize your partial credit.  
+ * */
+
+/************************
+ * Feedback on the lab
+ ************************/
+
+/*
+ * Any comments/questions/suggestions/experiences that you would help us to
+ * improve the lab.
+ * */
+
+/************************
+ * References
+ ************************/
+
+/*
+ * List of collaborators involved including any online references/citations.
+ * */
---
+++ dmm.c
@@ -0,0 +1,76 @@
+#include <stdio.h> //needed for size_t
+#include <unistd.h> //needed for sbrk
+#include <assert.h> //For asserts
+#include "dmm.h"
+
+/* You can improve the below metadata structure using the concepts from Bryant
+ * and OHallaron book (chapter 9).
+ */
+
+typedef struct metadata {
+       /* size_t is the return type of the sizeof operator. Since the size of
+ 	* an object depends on the architecture and its implementation, size_t 
+	* is used to represent the maximum size of any object in the particular
+ 	* implementation. 
+	* size contains the size of the data object or the amount of free
+ 	* bytes 
+	*/
+	size_t size;
+	struct metadata* next;
+	struct metadata* prev; //What's the use of prev pointer?
+} metadata_t;
+
+/* freelist maintains all the blocks which are not in use; freelist is kept
+ * always sorted to improve the efficiency of coalescing 
+ */
+
+static metadata_t* freelist = NULL;
+
+void* dmalloc(size_t numbytes) {
+	if(freelist == NULL) { 			//Initialize through sbrk call first time
+		if(!dmalloc_init())
+			return NULL;
+	}
+
+	assert(numbytes > 0);
+
+	/* Your code goes here */
+	
+	return NULL;
+}
+
+void dfree(void* ptr) {
+
+	/* Your free and coalescing code goes here */
+}
+
+bool dmalloc_init() {
+
+	/* Two choices: 
+ 	* 1. Append prologue and epilogue blocks to the start and the end of the freelist
+ 	* 2. Initialize freelist pointers to NULL
+ 	*
+ 	* Note: We provide the code for 2. Using 1 will help you to tackle the
+ 	* corner cases succinctly.
+ 	*/
+
+	size_t max_bytes = ALIGN(MAX_HEAP_SIZE);
+	freelist = (metadata_t*) sbrk(max_bytes); // returns heap_region, which is initialized to freelist
+	/* Q: Why casting is used? i.e., why (void*)-1? */
+	if (freelist == (void *)-1)
+		return false;
+	freelist->next = NULL;
+	freelist->prev = NULL;
+	freelist->size = max_bytes-METADATA_T_ALIGNED;
+	return true;
+}
+
+/*Only for debugging purposes; can be turned off through -NDEBUG flag*/
+void print_freelist() {
+	metadata_t *freelist_head = freelist;
+	while(freelist_head != NULL) {
+		DEBUG("\tFreelist Size:%zd, Head:%p, Prev:%p, Next:%p\t",freelist_head->size,freelist_head,freelist_head->prev,freelist_head->next);
+		freelist_head = freelist_head->next;
+	}
+	DEBUG("\n");
+}
